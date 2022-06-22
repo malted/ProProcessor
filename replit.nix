19217fe7c58c2626e1af03d2764023c6316fb6b5
Malted <malted@malted.dev>
2022-06-22 20:18:04 +0100

    :wrench: Remove jest from nix config

--- replit.nix
+++ replit.nix
@@ -1,9 +1,8 @@
 { pkgs }: {
 	deps = [
 		pkgs.nodejs-16_x
-        pkgs.nodePackages.typescript-language-server
-        pkgs.nodePackages.yarn
-        pkgs.replitPackages.jest
-        pkgs.gh
+		pkgs.nodePackages.typescript-language-server
+		pkgs.nodePackages.yarn
+		pkgs.gh
 	];
 }
